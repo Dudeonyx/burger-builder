3260b544f6ee710599d4aa22a87c1f18fc1d1343
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2018-11-29 17:57:42 +0100

    Updates

---
+++ precache-manifest.e56ddce55c99745249dd927c907f5d4e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ac5aba3a8d8ce834930",
+    "url": "/burger-builder/static/css/4.05d00fb7.chunk.css"
+  },
+  {
+    "revision": "9469e700e7cce946723d",
+    "url": "/burger-builder/static/css/main.0f194774.chunk.css"
+  },
+  {
+    "revision": "22bae13302340e0d0388",
+    "url": "/burger-builder/static/js/runtime~main.22bae133.js"
+  },
+  {
+    "revision": "aa149f7c027c47837b7d",
+    "url": "/burger-builder/static/js/1.aa149f7c.chunk.js"
+  },
+  {
+    "revision": "eb3c6f1ea07c156d6172",
+    "url": "/burger-builder/static/js/7.eb3c6f1e.chunk.js"
+  },
+  {
+    "revision": "8d21702874b94bad1c76",
+    "url": "/burger-builder/static/js/2.8d217028.chunk.js"
+  },
+  {
+    "revision": "c2637c2e9b1708a745e0",
+    "url": "/burger-builder/static/js/6.c2637c2e.chunk.js"
+  },
+  {
+    "revision": "4ff5d9c668c99d07f579",
+    "url": "/burger-builder/static/js/3.4ff5d9c6.chunk.js"
+  },
+  {
+    "revision": "9469e700e7cce946723d",
+    "url": "/burger-builder/static/js/main.9469e700.chunk.js"
+  },
+  {
+    "revision": "0ac5aba3a8d8ce834930",
+    "url": "/burger-builder/static/js/4.0ac5aba3.chunk.js"
+  },
+  {
+    "revision": "53b51d7ae58878b0fcc7",
+    "url": "/burger-builder/static/js/5.53b51d7a.chunk.js"
+  },
+  {
+    "revision": "53b51d7ae58878b0fcc7",
+    "url": "/burger-builder/static/css/5.949d9176.chunk.css"
+  },
+  {
+    "revision": "4ff5d9c668c99d07f579",
+    "url": "/burger-builder/static/css/3.b6dc31ae.chunk.css"
+  },
+  {
+    "revision": "8d21702874b94bad1c76",
+    "url": "/burger-builder/static/css/2.fd0d8bb2.chunk.css"
+  },
+  {
+    "revision": "aa149f7c027c47837b7d",
+    "url": "/burger-builder/static/css/1.674ee5a1.chunk.css"
+  },
+  {
+    "revision": "1daff911fa5b033e277beaadd1b45f16",
+    "url": "/burger-builder/index.html"
+  }
+];
