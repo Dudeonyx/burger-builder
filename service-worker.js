778fe51e0140d9b6e44719c3095e3912cc370fa7
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2018-12-09 14:11:16 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/burger-builder/precache-manifest.a5a34e4ccadf70201048a4ceb5a6c4fa.js"
+  "/burger-builder/precache-manifest.b21fcc9fd5ab3de4efddbf5094db99b4.js"
 );
 
 workbox.clientsClaim();
