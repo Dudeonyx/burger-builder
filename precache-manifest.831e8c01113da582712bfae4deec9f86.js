8c83419c2a2e928a84938dbd13ac191d57e8195c
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2018-12-16 12:08:26 +0100

    Updates

---
+++ precache-manifest.831e8c01113da582712bfae4deec9f86.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = [
+  {
+    "revision": "beb6056edde8ed4d6445",
+    "url": "/burger-builder/static/css/Modal.9052854e.chunk.css"
+  },
+  {
+    "revision": "d4cdff4df7e14d5e2169",
+    "url": "/burger-builder/static/js/0.d4cdff4d.chunk.js"
+  },
+  {
+    "revision": "d36f3f40dda7d29c2ffe",
+    "url": "/burger-builder/static/js/BurgerDisplay.d36f3f40.chunk.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/26.1 burger-logo.png.b8503d26.png"
+  },
+  {
+    "revision": "bfdec66d02d276219a82",
+    "url": "/burger-builder/static/js/main.bfdec66d.chunk.js"
+  },
+  {
+    "revision": "ef288f21b4854ba243ce",
+    "url": "/burger-builder/static/js/BurgerBuilder.ef288f21.chunk.js"
+  },
+  {
+    "revision": "144fb421c24534b602b0",
+    "url": "/burger-builder/static/js/runtime~main.144fb421.js"
+  },
+  {
+    "revision": "cda0acdfa01726652d60",
+    "url": "/burger-builder/static/js/Checkout.cda0acdf.chunk.js"
+  },
+  {
+    "revision": "fc60d897cf31de1a17b7",
+    "url": "/burger-builder/static/js/Orders.fc60d897.chunk.js"
+  },
+  {
+    "revision": "c4835bd1d71f4f2afade",
+    "url": "/burger-builder/static/js/11.c4835bd1.chunk.js"
+  },
+  {
+    "revision": "b1cad9cbb99e86ced4ac",
+    "url": "/burger-builder/static/js/SideDrawer.b1cad9cb.chunk.js"
+  },
+  {
+    "revision": "2cb81f68d9ab86664cc7",
+    "url": "/burger-builder/static/js/OrderSummary.2cb81f68.chunk.js"
+  },
+  {
+    "revision": "beb6056edde8ed4d6445",
+    "url": "/burger-builder/static/js/Modal.beb6056e.chunk.js"
+  },
+  {
+    "revision": "dc0bc4e9fcd9d1e70136",
+    "url": "/burger-builder/static/js/immer.dc0bc4e9.chunk.js"
+  },
+  {
+    "revision": "0aaa00e536bdba88821a",
+    "url": "/burger-builder/static/js/BuildControls.0aaa00e5.chunk.js"
+  },
+  {
+    "revision": "bfdec66d02d276219a82",
+    "url": "/burger-builder/static/css/main.96335b0a.chunk.css"
+  },
+  {
+    "revision": "b1cad9cbb99e86ced4ac",
+    "url": "/burger-builder/static/css/SideDrawer.9052854e.chunk.css"
+  },
+  {
+    "revision": "2cb81f68d9ab86664cc7",
+    "url": "/burger-builder/static/css/OrderSummary.416f7faf.chunk.css"
+  },
+  {
+    "revision": "cda0acdfa01726652d60",
+    "url": "/burger-builder/static/css/Checkout.299d3b49.chunk.css"
+  },
+  {
+    "revision": "d36f3f40dda7d29c2ffe",
+    "url": "/burger-builder/static/css/BurgerDisplay.1f27a9df.chunk.css"
+  },
+  {
+    "revision": "0aaa00e536bdba88821a",
+    "url": "/burger-builder/static/css/BuildControls.6519cd28.chunk.css"
+  },
+  {
+    "revision": "310118cb27936a6b94927ed35439648a",
+    "url": "/burger-builder/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/burger-builder/precache-manifest.90db6a1c1aea8fdcda5757cd8582d40f.js"
+  "/burger-builder/precache-manifest.831e8c01113da582712bfae4deec9f86.js"
 );
 
 workbox.clientsClaim();
