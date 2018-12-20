9347271ba04b1c99a6a2caeb0745d6c06e3591f3
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2018-12-20 02:39:58 +0100

    Updates

---
+++ precache-manifest.e9674bea40a10a2e37e5cec94f526fca.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "595e22d0a1529d2a8b42",
+    "url": "/burger-builder/static/css/BurgerDisplay.5c25347b.chunk.css"
+  },
+  {
+    "revision": "f423080b6c5ba6192a7b",
+    "url": "/burger-builder/static/css/main.bbf8d3d5.chunk.css"
+  },
+  {
+    "revision": "6e9e974e10c0ad8a2dcc",
+    "url": "/burger-builder/static/js/Orders.6e9e974e.chunk.js"
+  },
+  {
+    "revision": "0c390cc5589b4207b786",
+    "url": "/burger-builder/static/js/Checkout.0c390cc5.chunk.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/26.1 burger-logo.png.b8503d26.png"
+  },
+  {
+    "revision": "246f9a4deffb5ff91120",
+    "url": "/burger-builder/static/js/BurgerBuilder.246f9a4d.chunk.js"
+  },
+  {
+    "revision": "80ae28ecf81964450532",
+    "url": "/burger-builder/static/js/SideDrawer.80ae28ec.chunk.js"
+  },
+  {
+    "revision": "f423080b6c5ba6192a7b",
+    "url": "/burger-builder/static/js/main.f423080b.chunk.js"
+  },
+  {
+    "revision": "595e22d0a1529d2a8b42",
+    "url": "/burger-builder/static/js/BurgerDisplay.595e22d0.chunk.js"
+  },
+  {
+    "revision": "36b87b2f324cc870f175",
+    "url": "/burger-builder/static/js/OrderSummary.36b87b2f.chunk.js"
+  },
+  {
+    "revision": "6f6d5d39c8543336abc5",
+    "url": "/burger-builder/static/js/7.6f6d5d39.chunk.js"
+  },
+  {
+    "revision": "84a09d93f7feb2f2a14e",
+    "url": "/burger-builder/static/js/runtime~main.84a09d93.js"
+  },
+  {
+    "revision": "246f9a4deffb5ff91120",
+    "url": "/burger-builder/static/css/BurgerBuilder.8c403a67.chunk.css"
+  },
+  {
+    "revision": "7253fcc9ca6d8b38f9b0b2d5d438aa71",
+    "url": "/burger-builder/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/burger-builder/precache-manifest.5e5a18a1b26e067e4f5f57cf3ca7054a.js"
+  "/burger-builder/precache-manifest.e9674bea40a10a2e37e5cec94f526fca.js"
 );
 
 workbox.clientsClaim();
