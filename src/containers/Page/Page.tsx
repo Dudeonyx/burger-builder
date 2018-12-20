a29340201bf5e1499d56801a3d034b6b9593fad7
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2018-12-20 01:42:24 +0100

    other Page

--- src/containers/Page/Page.tsx
+++ src/containers/Page/Page.tsx
@@ -1,24 +1,15 @@
 import React, { FunctionComponent, lazy } from 'react';
-import { suspenseNode2 } from '../../HOCs/suspensed';
 import { Switch, Route } from 'react-router-dom';
-const Orders = lazy(() =>
-  import(/* webpackChunkName: "Orders" */ '../Orders/Orders'),
-);
-const Checkout = lazy(() =>
-  import(/* webpackChunkName: "Checkout" */ '../Checkout/Checkout'),
-);
-const BurgerBuilder = lazy(() =>
-  import(/* webpackChunkName: "BurgerBuilder" */ '../BurgerBuilder/BurgerBuilder'),
-);
-const SBurgerBuilder = suspenseNode2(BurgerBuilder);
-const SOrders = suspenseNode2(Orders);
-const SCheckout = suspenseNode2(Checkout);
+import BurgerBuilder from '../BurgerBuilder/BurgerBuilder';
+import Orders from '../Orders/Orders';
+import Checkout from '../Checkout/Checkout';
+
 const Page: FunctionComponent = () => {
   return (
     <Switch>
-      <Route path="/" exact={true} render={p => SBurgerBuilder(p)} />
-      <Route path="/all-orders" exact={true} render={p => SOrders(p)} />
-      <Route path="/checkout" exact={false} render={p => SCheckout(p)} />
+      <Route path="/" exact={true} component={BurgerBuilder} />
+      <Route path="/all-orders" exact={true} component={Orders} />
+      <Route path="/checkout" exact={false} component={Checkout} />
     </Switch>
   );
 };
